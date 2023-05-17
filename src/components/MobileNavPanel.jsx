a83e1319c7c8e0205955bd3a6f6e789d8de25211
RoKen1000 <rosskennedy993@outlook.com>
2023-05-17 19:38:38 +0100

    Finish adding functionality to the movile nav panel.

--- src/components/MobileNavPanel.jsx
+++ src/components/MobileNavPanel.jsx
@@ -1,8 +1,9 @@
-import { faBars, faCode, faEnvelope, faFolderOpen, faUser } from "@fortawesome/free-solid-svg-icons"
+import { faBars, faCode, faEnvelope, faFolderOpen, faHome, faUser } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useState } from "react"
 import { Offcanvas } from "react-bootstrap"
 import { NavLink } from "react-router-dom"
+import "../styles/MobileNavPanel.css"
 
 export const MobileNavPanel = () => {
     const [show, setShow] = useState(false)
@@ -18,26 +19,26 @@ export const MobileNavPanel = () => {
                     <Offcanvas.Title>Where to?</Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
-                    <div>
-                        <NavLink to="/about">
-                            <FontAwesomeIcon icon={faUser} className="icons" size="3x"/>
+                        <NavLink to="/about" className="mobile-nav-link" onClick={handleClose} id="top">
+                            <FontAwesomeIcon icon={faUser} className="mobile-nav-icons" size="3x"/>
+                            <p>About</p>
                         </NavLink>
-                    </div>
-                    <div>
-                        <NavLink to="/tech-skills">
-                            <FontAwesomeIcon icon={faCode} className="icons" size="3x"/>
+                        <NavLink to="/tech-skills" className="mobile-nav-link" onClick={handleClose}>
+                            <FontAwesomeIcon icon={faCode} className="mobile-nav-icons" size="3x"/>
+                            <p>Skills</p>
                         </NavLink>
-                    </div>
-                    <div>
-                        <NavLink to="/projects">
-                            <FontAwesomeIcon icon={faFolderOpen} className="icons" size="3x"/>
+                        <NavLink to="/projects" className="mobile-nav-link" onClick={handleClose}>
+                            <FontAwesomeIcon icon={faFolderOpen} className="mobile-nav-icons" size="3x"/>
+                            <p>Projects</p>
                         </NavLink>
-                    </div>
-                    <div>
-                        <NavLink to="/contact">
-                            <FontAwesomeIcon icon={faEnvelope} className="icons" size="3x"/>
+                        <NavLink to="/contact" className="mobile-nav-link" onClick={handleClose}>
+                            <FontAwesomeIcon icon={faEnvelope} className="mobile-nav-icons" size="3x"/>
+                            <p>Contact</p>
                         </NavLink>
-                    </div>
+                        <NavLink to="/" className="mobile-nav-link" onClick={handleClose}>
+                            <FontAwesomeIcon icon={faHome} className="mobile-nav-icons" size="3x"/>
+                            <p>Home</p>
+                        </NavLink>
                 </Offcanvas.Body>
             </Offcanvas>
         </div>
