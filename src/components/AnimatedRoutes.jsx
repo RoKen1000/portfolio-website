6f71684a08f330bb9eed39c025e5230483eebe17
RoKen1000 <rosskennedy993@outlook.com>
2023-05-18 17:07:28 +0100

    Create grid and grid cards with stock text as well as use icons in cards. Make card positioning responsive.

--- src/components/AnimatedRoutes.jsx
+++ src/components/AnimatedRoutes.jsx
@@ -2,6 +2,7 @@ import { useLocation, Route, Routes } from "react-router-dom"
 import { Home } from "./Home"
 import { About } from "./About"
 import { AnimatePresence } from "framer-motion"
+import { TechSkills } from "./TechSkills"
 
 export const AnimatedRoutes = () => {
 
@@ -12,7 +13,7 @@ export const AnimatedRoutes = () => {
             <Routes location={location} key={location.pathname}>
                 <Route path="/" element={<Home />}/>
                 <Route path="/about" element={<About />}/>
-                <Route path="/tech-skills" />
+                <Route path="/tech-skills" element={<TechSkills />}/>
                 <Route path="/projects" />
                 <Route path="/contact" />
             </Routes>
