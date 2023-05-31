b853349dad26cd70996dde8cd8cff8efb0f3eb19
RoKen1000 <rosskennedy993@outlook.com>
2023-05-31 19:47:25 +0100

    Make method of setting desktop nav bar button clicked state uniform with HomeNavImages in the DesktopNavBar component. Remove this functionality for the link button back to the home page as it was redundant.

--- src/components/DesktopNavBar.jsx
+++ src/components/DesktopNavBar.jsx
@@ -1,21 +1,18 @@
 import { NavLink } from "react-router-dom"
 import "../styles/DesktopNavBar.css"
 import "../styles/ClickedButton.css"
-import { useState } from "react"
 
 export const DesktopNavBar = ({buttonClicked, setButtonClicked}) => {
 
-    // const [buttonClicked, setButtonClicked] = useState("")
-
-    const handleClick = (e) => setButtonClicked(e.target.id);
+    const handleClick = (button) => setButtonClicked(button);
 
     return(
         <nav className="desktop-nav-link-container">
-            <NavLink className={buttonClicked === "about-nav-button" ? "clicked-button" : null} onClick={handleClick} id="about-nav-button" to="/about">About</NavLink>
-            <NavLink className={buttonClicked === "skills-nav-button" ? "clicked-button" : null} onClick={handleClick} id="skills-nav-button" to="/skills">Skills</NavLink>
-            <NavLink className={buttonClicked === "portfolio-nav-button" ? "clicked-button" : null} onClick={handleClick} id="portfolio-nav-button" to="/portfolio">Portfolio</NavLink>
-            <NavLink className={buttonClicked === "contact-nav-button" ? "clicked-button" : null} onClick={handleClick} id="contact-nav-button" to="/contact">Contact</NavLink>
-            <NavLink className={buttonClicked === "home-nav-button" ? "clicked-button" : null} onClick={handleClick} id="home-nav-button" to="/">Home</NavLink>             
+            <NavLink className={buttonClicked === "about-nav-button" ? "clicked-button" : null} onClick={() => handleClick("about-nav-button")} to="/about">About</NavLink>
+            <NavLink className={buttonClicked === "skills-nav-button" ? "clicked-button" : null} onClick={() => handleClick("skills-nav-button")} to="/skills">Skills</NavLink>
+            <NavLink className={buttonClicked === "portfolio-nav-button" ? "clicked-button" : null} onClick={() => handleClick("portfolio-nav-button")} to="/portfolio">Portfolio</NavLink>
+            <NavLink className={buttonClicked === "contact-nav-button" ? "clicked-button" : null} onClick={() => handleClick("contact-nav-button")} to="/contact">Contact</NavLink>
+            <NavLink to="/">Home</NavLink>             
         </nav>
     )
 }
