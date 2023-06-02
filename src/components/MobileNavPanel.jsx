e2f5127140611f2b852279eb2fe98c4e60498489
RoKen1000 <rosskennedy993@outlook.com>
2023-06-02 12:13:44 +0100

    Add text content to about me page and improve its styling to be more responsive to different screen widths when factoring in the new grid container.

--- src/components/MobileNavPanel.jsx
+++ src/components/MobileNavPanel.jsx
@@ -20,23 +20,28 @@ export const MobileNavPanel = () => {
                 </Offcanvas.Header>
                 <Offcanvas.Body>
                         <NavLink to="/about" className="mobile-nav-link" onClick={handleClose} id="top">
-                            <FontAwesomeIcon icon={faUser} className="mobile-nav-icons" size="3x"/>
+                            <FontAwesomeIcon icon={faUser} className="mobile-nav-icons"
+                            alt="Icon of a person linking to the about page." size="3x"/>
                             <p>About</p>
                         </NavLink>
                         <NavLink to="/skills" className="mobile-nav-link" onClick={handleClose}>
-                            <FontAwesomeIcon icon={faCode} className="mobile-nav-icons" size="3x"/>
+                            <FontAwesomeIcon icon={faCode} className="mobile-nav-icons"
+                            alt="Icon of a code bracket linking to the skills page." size="3x"/>
                             <p>Skills</p>
                         </NavLink>
                         <NavLink to="/portfolio" className="mobile-nav-link" onClick={handleClose}>
-                            <FontAwesomeIcon icon={faFolderOpen} className="mobile-nav-icons" size="3x"/>
+                            <FontAwesomeIcon icon={faFolderOpen} className="mobile-nav-icons"
+                            alt="Icon of a folder linking to the portfolio page." size="3x"/>
                             <p>Portfolio</p>
                         </NavLink>
                         <NavLink to="/contact" className="mobile-nav-link" onClick={handleClose}>
-                            <FontAwesomeIcon icon={faEnvelope} className="mobile-nav-icons" size="3x"/>
+                            <FontAwesomeIcon icon={faEnvelope} className="mobile-nav-icons"
+                            alt="Icon of an envelope linking to the contact page." size="3x"/>
                             <p>Contact</p>
                         </NavLink>
                         <NavLink to="/" className="mobile-nav-link" onClick={handleClose}>
-                            <FontAwesomeIcon icon={faHome} className="mobile-nav-icons" size="3x"/>
+                            <FontAwesomeIcon icon={faHome} className="mobile-nav-icons"
+                            alt="Icon of a house linking to the home page." size="3x"/>
                             <p>Home</p>
                         </NavLink>
                 </Offcanvas.Body>
