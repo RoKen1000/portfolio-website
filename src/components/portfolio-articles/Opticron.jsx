c79ef9f6c208661f783a5d8a227e72ccf2a06658
RoKen1000 <rosskennedy993@outlook.com>
2023-10-06 16:41:35 +0100

    Minor spelling corrections on the Opticron article page.

--- src/components/portfolio-articles/Opticron.jsx
+++ src/components/portfolio-articles/Opticron.jsx
@@ -38,7 +38,7 @@ export const Opticron = () => {
                         <img id="opticron-screenshot" src={windowWidth < 600 ? opticronMob : opticronDesk} alt="Screenshot of some articles on the Optricron website."></img>
                         <figcaption>Screenshot of articles on the Optricron website.</figcaption>
                     </figure>
-                    <p>This was a website I developed as part of a tech test. This was my frst real project getting to grips with C#, .NET Core and ASP.NET, and was a tremendously valuable learning opportunity. It features a main page for the website as well as a content management system that can be used to perform CRUD operations to control some of the elements shown on the main page.</p>
+                    <p>This was a website I developed as part of a tech test. This was my first real project getting to grips with C#, .NET Core and ASP.NET, and was a tremendously valuable learning opportunity. It features a main page for the website as well as a content management system that can be used to perform CRUD operations to control some of the elements shown on the main page.</p>
                     <p>The tech stack for this project is:</p>
                     <ul className="portfolio-article-tech-stack">
                         <li>C#</li>
@@ -50,8 +50,8 @@ export const Opticron = () => {
                         <li>HTML</li>
                         <li>CSS</li>
                     </ul>
-                    <p>The project also makes use of the front-end libraries Foundation Framework and Slick.js. The carousel effect of Slick.js is used to animate and cycle through the various different categories and their associated images. Foundation Framework's grid system was used to responsively space the various articles and special offeres found in the main page.</p> 
-                    <p>The database was set up using a code-first design paradigm. There are class objects for the categories, articles and special offers.</p>
+                    <p>The project also makes use of the front-end libraries Foundation Framework and Slick.js. The carousel effect of Slick.js is used to animate and cycle through the various different categories and their associated images. Foundation Framework's grid system was used to responsively space the various articles and special offers found in the main page.</p> 
+                    <p>The database was set up using a code-first design paradigm. There are class objects for the categories, articles and special offers which then are used to make up the various SQL tables.</p>
                 </article>
                 <Button onClick={() => navigate("/portfolio")}>Back To Portfolio</Button>
             </motion.div>
