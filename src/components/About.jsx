595907284179800c961ed103d9ebef6d73705a43
RoKen1000 <rosskennedy993@outlook.com>
2023-12-21 11:37:51 +0000

    Add spacer to About section so that content does not run behind the footer in mobile views.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,6 +18,7 @@ export const About = () => {
                         <p>As part of the course <strong>I worked on several Full-Stack projects</strong> which can be read about in the Portfolio section of this site. Having graduated from the boot camp <strong>I am based in the East Midlands (Kettering).</strong> In the meantime I am further developing the skills taught to me on the bootcamp and <strong>I am expanding my knowledge and experience in Full-Stack development by developing with C#, .NET Core and ASP.NET.</strong></p>
                         <p id="about-final-paragraph">When I am not developing my programming skills I like to learn foreign languages. I am fluent in French and I am keenly interested in Latin. I learned these languages as part of my life-long interest in history. I am also an avid music fan and like to write and record my own music.</p>
                     </div>
+                    <div id="about-bottom-spacer"></div>
                 </motion.div>
         </main>
     )
