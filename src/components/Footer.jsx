28966b4493780a9269b956398d23150fbd814f2a
RoKen1000 <rosskennedy993@outlook.com>
2023-12-21 10:03:27 +0000

    Remove unused imports in Footer component. Rename actual component in file to Footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,8 @@
-import {motion} from "framer-motion"
-import { Button } from "react-bootstrap"
 import "../styles/ContactPage.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
-export const ContactPage = () => {
+export const Footer = () => {
 
     return(
         <footer>       
