e0459c39c4571ca07f8a4b7df46445ac90fb6bf3
RoKen1000 <rosskennedy993@outlook.com>
2023-12-21 10:06:11 +0000

    Rename ContactPage stylesheet to Footer. Remove styling for the now defunct contact form that would have been on the old ContantPage component.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import "../styles/ContactPage.css"
+import "../styles/Footer.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
