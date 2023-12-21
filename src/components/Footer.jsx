6e7c808ab1f19b9467cba39f14bad04964fba67d
RoKen1000 <rosskennedy993@outlook.com>
2023-12-21 10:35:08 +0000

    Create footer-icons class for the icons contained in Footer. Add styling in Footer.css to make the background colour of the footer same colour as header. Make icons change colour on hover for responsiveness.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,11 @@ export const Footer = () => {
     return(
         <footer>       
                 <div className="social-icons">
-                    <a href="https://github.com/RoKen1000" rel="noreferrer" target="_blank" name="GitHub">
+                    <a href="https://github.com/RoKen1000" rel="noreferrer" target="_blank" name="GitHub" className="footer-icons">
                         <FontAwesomeIcon icon={faGithub} size="3x" alt="GitHub"/>
                     </a>
                     <a href="https://www.linkedin.com/in/ross-kennedy-2735a7237/" rel="noreferrer" target="_blank" name="LinkedIn">
-                        <FontAwesomeIcon icon={faLinkedin} size="3x" alt="LinkedIn"/>
+                        <FontAwesomeIcon icon={faLinkedin} size="3x" alt="LinkedIn" className="footer-icons"/>
                     </a>
                 </div>
         </footer>
